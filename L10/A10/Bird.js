71a16b77075891fd7c87fc4ff1d80c2544f4b051
Markus Damm <44026453+MarkusDamm@users.noreply.github.com>
2019-12-18 17:07:26 +0100

    updated Birds

--- L10/A10/Bird.js
+++ L10/A10/Bird.js
@@ -7,103 +7,95 @@ var MyFuwa_10;
             this.velocity = new MyFuwa_10.Vector(2 + Math.random() * 4, -2 + Math.random() * 4);
             this.colorBody = this.getRandomColor();
             this.colorTail = this.getRandomColor();
-            if (this.position.y <= 480)
-                this.drawFly();
-            else
-                this.drawStand();
+            this.draw();
             this.depth = false;
-            if (Math.random() <= 0.4) {
+            if (Math.random() <= 0.3) {
                 this.depth = true;
             }
         }
-        drawFly() {
+        draw() {
             // console.log("Draw Bird");
             MyFuwa_10.crc2.save();
             MyFuwa_10.crc2.translate(this.position.x, this.position.y);
             MyFuwa_10.crc2.scale(0.8, 0.8);
             MyFuwa_10.crc2.beginPath();
-            //Beak
-            MyFuwa_10.crc2.fillStyle = "black";
-            MyFuwa_10.crc2.beginPath();
-            MyFuwa_10.crc2.moveTo(65, 0);
-            MyFuwa_10.crc2.lineTo(50, -8);
-            MyFuwa_10.crc2.lineTo(50, 8);
-            MyFuwa_10.crc2.closePath();
-            MyFuwa_10.crc2.fill();
-            //Tail
-            MyFuwa_10.crc2.fillStyle = this.colorTail;
-            MyFuwa_10.crc2.beginPath();
-            MyFuwa_10.crc2.moveTo(-20, 0);
-            MyFuwa_10.crc2.lineTo(-40, -5);
-            MyFuwa_10.crc2.lineTo(-40, 5);
-            MyFuwa_10.crc2.closePath();
-            MyFuwa_10.crc2.fill();
-            //Backwing
-            MyFuwa_10.crc2.fillStyle = this.colorBody;
-            MyFuwa_10.crc2.beginPath();
-            MyFuwa_10.crc2.moveTo(10, 0);
-            MyFuwa_10.crc2.quadraticCurveTo(30, -20, 20, -45);
-            MyFuwa_10.crc2.quadraticCurveTo(0, -22, 10, 0);
-            MyFuwa_10.crc2.closePath();
-            MyFuwa_10.crc2.stroke();
-            MyFuwa_10.crc2.fill();
-            //Body
-            MyFuwa_10.crc2.beginPath();
-            MyFuwa_10.crc2.arc(43, 0, 12, Math.PI * 0.9, Math.PI * 1, true);
-            MyFuwa_10.crc2.moveTo(43, 0);
-            MyFuwa_10.crc2.quadraticCurveTo(0, -25, -25, 0);
-            MyFuwa_10.crc2.quadraticCurveTo(0, 20, 43, 0);
-            MyFuwa_10.crc2.stroke();
-            MyFuwa_10.crc2.fill();
-            MyFuwa_10.crc2.closePath();
-            //Frontwing
-            MyFuwa_10.crc2.beginPath();
-            MyFuwa_10.crc2.moveTo(10, 0);
-            MyFuwa_10.crc2.quadraticCurveTo(20, -20, 0, -45);
-            MyFuwa_10.crc2.quadraticCurveTo(-10, -22, 10, 0);
-            MyFuwa_10.crc2.closePath();
-            MyFuwa_10.crc2.stroke();
-            MyFuwa_10.crc2.fill();
-            MyFuwa_10.crc2.scale(1, 1);
-            MyFuwa_10.crc2.restore();
-        }
-        drawStand() {
-            MyFuwa_10.crc2.save();
-            MyFuwa_10.crc2.translate(this.position.x, this.position.y);
-            MyFuwa_10.crc2.scale(0.8, 0.8);
-            MyFuwa_10.crc2.beginPath();
-            //Feet
-            MyFuwa_10.crc2.strokeStyle = "black";
-            MyFuwa_10.crc2.lineWidth = 2;
-            MyFuwa_10.crc2.moveTo(0, -30);
-            MyFuwa_10.crc2.lineTo(-10, 0);
-            MyFuwa_10.crc2.lineTo(0, -30);
-            MyFuwa_10.crc2.lineTo(10, 0);
-            MyFuwa_10.crc2.stroke();
-            MyFuwa_10.crc2.closePath();
-            //Beak
-            MyFuwa_10.crc2.fillStyle = "black";
-            MyFuwa_10.crc2.beginPath();
-            MyFuwa_10.crc2.moveTo(50, -50);
-            MyFuwa_10.crc2.lineTo(35, -58);
-            MyFuwa_10.crc2.lineTo(35, -42);
-            MyFuwa_10.crc2.closePath();
-            MyFuwa_10.crc2.fill();
-            //Tail
-            MyFuwa_10.crc2.fillStyle = this.colorTail;
-            MyFuwa_10.crc2.beginPath();
-            MyFuwa_10.crc2.moveTo(-10, -30);
-            MyFuwa_10.crc2.lineTo(-40, -12);
-            MyFuwa_10.crc2.lineTo(-37, -4);
-            MyFuwa_10.crc2.closePath();
-            MyFuwa_10.crc2.fill();
-            //Body
-            MyFuwa_10.crc2.fillStyle = this.colorBody;
-            MyFuwa_10.crc2.beginPath();
-            MyFuwa_10.crc2.moveTo(30, -50);
-            MyFuwa_10.crc2.arc(30, -50, 12, Math.PI * 0.7, Math.PI * 1.1, true);
-            MyFuwa_10.crc2.quadraticCurveTo(-10, -50, -25, -20);
-            MyFuwa_10.crc2.quadraticCurveTo(10, -10, 30, -50);
+            if (this.position.y <= 550) {
+                //Beak
+                MyFuwa_10.crc2.fillStyle = "black";
+                MyFuwa_10.crc2.beginPath();
+                MyFuwa_10.crc2.moveTo(65, 0);
+                MyFuwa_10.crc2.lineTo(50, -8);
+                MyFuwa_10.crc2.lineTo(50, 8);
+                MyFuwa_10.crc2.closePath();
+                MyFuwa_10.crc2.fill();
+                //Tail
+                MyFuwa_10.crc2.fillStyle = this.colorTail;
+                MyFuwa_10.crc2.beginPath();
+                MyFuwa_10.crc2.moveTo(-20, 0);
+                MyFuwa_10.crc2.lineTo(-40, -5);
+                MyFuwa_10.crc2.lineTo(-40, 5);
+                MyFuwa_10.crc2.closePath();
+                MyFuwa_10.crc2.fill();
+                //Backwing
+                MyFuwa_10.crc2.fillStyle = this.colorBody;
+                MyFuwa_10.crc2.beginPath();
+                MyFuwa_10.crc2.moveTo(10, 0);
+                MyFuwa_10.crc2.quadraticCurveTo(30, -20, 20, -45);
+                MyFuwa_10.crc2.quadraticCurveTo(0, -22, 10, 0);
+                MyFuwa_10.crc2.closePath();
+                MyFuwa_10.crc2.stroke();
+                MyFuwa_10.crc2.fill();
+                //Body
+                MyFuwa_10.crc2.beginPath();
+                MyFuwa_10.crc2.arc(43, 0, 12, Math.PI * 0.9, Math.PI * 1, true);
+                MyFuwa_10.crc2.moveTo(43, 0);
+                MyFuwa_10.crc2.quadraticCurveTo(0, -25, -25, 0);
+                MyFuwa_10.crc2.quadraticCurveTo(0, 20, 43, 0);
+                MyFuwa_10.crc2.stroke();
+                MyFuwa_10.crc2.fill();
+                MyFuwa_10.crc2.closePath();
+                //Frontwing
+                MyFuwa_10.crc2.beginPath();
+                MyFuwa_10.crc2.moveTo(10, 0);
+                MyFuwa_10.crc2.quadraticCurveTo(20, -20, 0, -45);
+                MyFuwa_10.crc2.quadraticCurveTo(-10, -22, 10, 0);
+                MyFuwa_10.crc2.closePath();
+                MyFuwa_10.crc2.stroke();
+            }
+            else {
+                //Feet
+                MyFuwa_10.crc2.strokeStyle = "black";
+                MyFuwa_10.crc2.lineWidth = 2;
+                MyFuwa_10.crc2.moveTo(0, -30);
+                MyFuwa_10.crc2.lineTo(-10, 0);
+                MyFuwa_10.crc2.lineTo(0, -30);
+                MyFuwa_10.crc2.lineTo(10, 0);
+                MyFuwa_10.crc2.stroke();
+                MyFuwa_10.crc2.closePath();
+                //Beak
+                MyFuwa_10.crc2.fillStyle = "black";
+                MyFuwa_10.crc2.beginPath();
+                MyFuwa_10.crc2.moveTo(50, -50);
+                MyFuwa_10.crc2.lineTo(35, -58);
+                MyFuwa_10.crc2.lineTo(35, -42);
+                MyFuwa_10.crc2.closePath();
+                MyFuwa_10.crc2.fill();
+                //Tail
+                MyFuwa_10.crc2.fillStyle = this.colorTail;
+                MyFuwa_10.crc2.beginPath();
+                MyFuwa_10.crc2.moveTo(-10, -30);
+                MyFuwa_10.crc2.lineTo(-40, -12);
+                MyFuwa_10.crc2.lineTo(-37, -4);
+                MyFuwa_10.crc2.closePath();
+                MyFuwa_10.crc2.fill();
+                //Body
+                MyFuwa_10.crc2.fillStyle = this.colorBody;
+                MyFuwa_10.crc2.beginPath();
+                MyFuwa_10.crc2.moveTo(30, -50);
+                MyFuwa_10.crc2.arc(30, -50, 12, Math.PI * 0.7, Math.PI * 1.1, true);
+                MyFuwa_10.crc2.quadraticCurveTo(-10, -50, -25, -20);
+                MyFuwa_10.crc2.quadraticCurveTo(10, -10, 30, -50);
+            }
             MyFuwa_10.crc2.fill();
             MyFuwa_10.crc2.scale(1, 1);
             MyFuwa_10.crc2.restore();
@@ -115,13 +107,6 @@ var MyFuwa_10;
                 color += letters[Math.floor(Math.random() * 16)];
             }
             return color;
-        }
-        update() {
-            this.move();
-            if (this.position.y <= 480)
-                this.drawFly();
-            else
-                this.drawStand();
         }
     }
     MyFuwa_10.Bird = Bird;
--- L10/A10/Bird.ts
+++ L10/A10/Bird.ts
@@ -10,17 +10,14 @@ namespace MyFuwa_10 {
             this.velocity = new Vector(2 + Math.random() * 4, -2 + Math.random() * 4);
             this.colorBody = this.getRandomColor();
             this.colorTail = this.getRandomColor();
-            if (this.position.y <= 480)
-                this.drawFly();
-            else
-                this.drawStand();
+            this.draw();
             this.depth = false;
-            if (Math.random() <= 0.4) {
+            if (Math.random() <= 0.3) {
                 this.depth = true;
             }
         }
 
-        drawFly(): void {
+        draw(): void {
             // console.log("Draw Bird");
 
             crc2.save();
@@ -28,105 +25,94 @@ namespace MyFuwa_10 {
             crc2.scale(0.8, 0.8);
             crc2.beginPath();
 
-            //Beak
-            crc2.fillStyle = "black";
-            crc2.beginPath();
-            crc2.moveTo(65, 0);
-            crc2.lineTo(50, -8);
-            crc2.lineTo(50, 8);
-            crc2.closePath();
-            crc2.fill();
+            if (this.position.y <= 550) {
+                //Beak
+                crc2.fillStyle = "black";
+                crc2.beginPath();
+                crc2.moveTo(65, 0);
+                crc2.lineTo(50, -8);
+                crc2.lineTo(50, 8);
+                crc2.closePath();
+                crc2.fill();
 
-            //Tail
-            crc2.fillStyle = this.colorTail;
-            crc2.beginPath();
-            crc2.moveTo(-20, 0);
-            crc2.lineTo(-40, -5);
-            crc2.lineTo(-40, 5);
-            crc2.closePath();
-            crc2.fill();
+                //Tail
+                crc2.fillStyle = this.colorTail;
+                crc2.beginPath();
+                crc2.moveTo(-20, 0);
+                crc2.lineTo(-40, -5);
+                crc2.lineTo(-40, 5);
+                crc2.closePath();
+                crc2.fill();
 
-            //Backwing
-            crc2.fillStyle = this.colorBody;
-            crc2.beginPath();
-            crc2.moveTo(10, 0);
-            crc2.quadraticCurveTo(30, -20, 20, -45);
-            crc2.quadraticCurveTo(0, -22, 10, 0);
-            crc2.closePath();
-            crc2.stroke();
-            crc2.fill();
+                //Backwing
+                crc2.fillStyle = this.colorBody;
+                crc2.beginPath();
+                crc2.moveTo(10, 0);
+                crc2.quadraticCurveTo(30, -20, 20, -45);
+                crc2.quadraticCurveTo(0, -22, 10, 0);
+                crc2.closePath();
+                crc2.stroke();
+                crc2.fill();
 
-            //Body
-            crc2.beginPath();
-            crc2.arc(43, 0, 12, Math.PI * 0.9, Math.PI * 1, true);
-            crc2.moveTo(43, 0);
-            crc2.quadraticCurveTo(0, -25, -25, 0);
-            crc2.quadraticCurveTo(0, 20, 43, 0);
-            crc2.stroke();
-            crc2.fill();
-            crc2.closePath();
+                //Body
+                crc2.beginPath();
+                crc2.arc(43, 0, 12, Math.PI * 0.9, Math.PI * 1, true);
+                crc2.moveTo(43, 0);
+                crc2.quadraticCurveTo(0, -25, -25, 0);
+                crc2.quadraticCurveTo(0, 20, 43, 0);
+                crc2.stroke();
+                crc2.fill();
+                crc2.closePath();
 
-            //Frontwing
-            crc2.beginPath();
-            crc2.moveTo(10, 0);
-            crc2.quadraticCurveTo(20, -20, 0, -45);
-            crc2.quadraticCurveTo(-10, -22, 10, 0);
-            crc2.closePath();
-            crc2.stroke();
-            crc2.fill();
+                //Frontwing
+                crc2.beginPath();
+                crc2.moveTo(10, 0);
+                crc2.quadraticCurveTo(20, -20, 0, -45);
+                crc2.quadraticCurveTo(-10, -22, 10, 0);
+                crc2.closePath();
+                crc2.stroke();
+            }
+            else {
+                //Feet
+                crc2.strokeStyle = "black";
+                crc2.lineWidth = 2;
+                crc2.moveTo(0, -30);
+                crc2.lineTo(-10, 0);
+                crc2.lineTo(0, -30);
+                crc2.lineTo(10, 0);
+                crc2.stroke();
+                crc2.closePath();
 
+                //Beak
+                crc2.fillStyle = "black";
+                crc2.beginPath();
+                crc2.moveTo(50, -50);
+                crc2.lineTo(35, -58);
+                crc2.lineTo(35, -42);
+                crc2.closePath();
+                crc2.fill();
 
-            crc2.scale(1, 1);
-            crc2.restore();
-        }
+                //Tail
+                crc2.fillStyle = this.colorTail;
+                crc2.beginPath();
+                crc2.moveTo(-10, -30);
+                crc2.lineTo(-40, -12);
+                crc2.lineTo(-37, -4);
+                crc2.closePath();
+                crc2.fill();
 
-        drawStand(): void {
-            crc2.save();
-            crc2.translate(this.position.x, this.position.y);
-            crc2.scale(0.8, 0.8);
-            crc2.beginPath();
-
-            //Feet
-            crc2.strokeStyle = "black";
-            crc2.lineWidth = 2;
-            crc2.moveTo(0, -30);
-            crc2.lineTo(-10, 0);
-            crc2.lineTo(0, -30);
-            crc2.lineTo(10, 0);
-            crc2.stroke();
-            crc2.closePath();
-
-            //Beak
-            crc2.fillStyle = "black";
-            crc2.beginPath();
-            crc2.moveTo(50, -50);
-            crc2.lineTo(35, -58);
-            crc2.lineTo(35, -42);
-            crc2.closePath();
-            crc2.fill();
-
-            //Tail
-            crc2.fillStyle = this.colorTail;
-            crc2.beginPath();
-            crc2.moveTo(-10, -30);
-            crc2.lineTo(-40, -12);
-            crc2.lineTo(-37, -4);
-            crc2.closePath();
-            crc2.fill();
-
-            //Body
-            crc2.fillStyle = this.colorBody;
-            crc2.beginPath();
-            crc2.moveTo(30, -50);
-            crc2.arc(30, -50, 12, Math.PI * 0.7, Math.PI * 1.1, true);
-            crc2.quadraticCurveTo(-10, -50, -25, -20);
-            crc2.quadraticCurveTo(10, -10, 30, -50);
-
+                //Body
+                crc2.fillStyle = this.colorBody;
+                crc2.beginPath();
+                crc2.moveTo(30, -50);
+                crc2.arc(30, -50, 12, Math.PI * 0.7, Math.PI * 1.1, true);
+                crc2.quadraticCurveTo(-10, -50, -25, -20);
+                crc2.quadraticCurveTo(10, -10, 30, -50);
+            }
             crc2.fill();
 
             crc2.scale(1, 1);
             crc2.restore();
-
         }
 
         getRandomColor(): string {
@@ -136,14 +122,6 @@ namespace MyFuwa_10 {
                 color += letters[Math.floor(Math.random() * 16)];
             }
             return color;
-        }
-
-        update(): void {
-            this.move();
-            if (this.position.y <= 480)
-                this.drawFly();
-            else
-                this.drawStand();
         }
     }
 }
