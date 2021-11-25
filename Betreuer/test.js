caadc63e7b7d1773fb06e6bb575a819461c2e926
Markus Damm <44026453+MarkusDamm@users.noreply.github.com>
2021-11-25 13:40:46 +0100

    draw a fir

--- Betreuer/test.js
+++ Betreuer/test.js
@@ -1,14 +1,46 @@
 "use strict";
 var Betreuer;
 (function (Betreuer) {
-    let word = "Hallo Welt";
-    console.log(word);
-    // prompt("type something");
-    word = prompt("type something", "type here");
-    console.log(word);
-    document.addEventListener("DOMContendLoaded", hdlLoad);
+    // document.addEventListener("DOMContendLoaded", hdlLoad);
+    window.addEventListener("load", hdlLoad);
+    let canvas;
+    let crc2;
     function hdlLoad() {
         // loading goes here
+        console.log("loading complete");
+        canvas = document.querySelector("canvas");
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        crc2 = canvas.getContext("2d");
+        drawFir();
+    }
+    function drawFir() {
+        crc2.save();
+        crc2.scale(5, 5);
+        crc2.translate(50, 100);
+        crc2.beginPath();
+        crc2.fillStyle = "brown";
+        crc2.fillRect(-5, -20, 10, 20);
+        crc2.closePath();
+        crc2.beginPath();
+        crc2.moveTo(0, -70);
+        crc2.lineTo(-23, -45);
+        crc2.lineTo(-13, -50);
+        crc2.lineTo(-27, -27);
+        crc2.lineTo(-14, -32);
+        crc2.lineTo(-35, -7);
+        // crc2.bezierCurveTo(-20, -25, -20, -30, -35, -7);
+        crc2.lineTo(0, -18);
+        crc2.lineTo(35, -7);
+        // crc2.bezierCurveTo(20, -25, 20, -30, 17, -32);
+        crc2.lineTo(14, -32);
+        crc2.lineTo(27, -27);
+        crc2.lineTo(13, -50);
+        crc2.lineTo(23, -45);
+        crc2.closePath();
+        crc2.fillStyle = "darkolivegreen";
+        crc2.fill();
+        crc2.restore();
     }
 })(Betreuer || (Betreuer = {}));
 //# sourceMappingURL=test.js.map
--- Betreuer/test.ts
+++ Betreuer/test.ts
@@ -1,15 +1,49 @@
 namespace Betreuer {
-  let word: string = "Hallo Welt";
-  console.log(word);
-  
-  // prompt("type something");
-  word = prompt("type something", "type here"); 
-  console.log(word);
-  
-  document.addEventListener("DOMContendLoaded", hdlLoad);
+  // document.addEventListener("DOMContendLoaded", hdlLoad);
+  window.addEventListener("load", hdlLoad);
+  let canvas: HTMLCanvasElement;
+  let crc2: CanvasRenderingContext2D;
 
 
   function hdlLoad(): void {
     // loading goes here
+    console.log("loading complete");
+    
+    canvas = <HTMLCanvasElement>document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    crc2 = <CanvasRenderingContext2D>canvas.getContext("2d");
+    drawFir();
+  }
+
+  function drawFir(): void {
+    crc2.save();
+    crc2.scale(5, 5);
+    crc2.translate(50, 100);
+    crc2.beginPath();
+    crc2.fillStyle = "brown";
+    crc2.fillRect(-5, -20, 10, 20);
+    crc2.closePath();
+
+    crc2.beginPath();
+    crc2.moveTo(0, -70);
+    crc2.lineTo(-23, -45);
+    crc2.lineTo(-13, -50);
+    crc2.lineTo(-27, -27);
+    crc2.lineTo(-14, -32);
+    crc2.lineTo(-35, -7);
+    // crc2.bezierCurveTo(-20, -25, -20, -30, -35, -7);
+    crc2.lineTo(0, -18);
+    
+    crc2.lineTo(35, -7);
+    // crc2.bezierCurveTo(20, -25, 20, -30, 17, -32);
+    crc2.lineTo(14, -32);
+    crc2.lineTo(27, -27);
+    crc2.lineTo(13, -50);
+    crc2.lineTo(23, -45);
+    crc2.closePath();
+    crc2.fillStyle = "darkolivegreen";
+    crc2.fill();
+    crc2.restore();
   }
 }
